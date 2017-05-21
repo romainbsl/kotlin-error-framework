f7bfe5d56087bc40f71da42483d759f2e13173d9
Romain Bsl <romainbsl@users.noreply.github.com>
2017-05-21 10:38:42 +0200

    create "Immutable" ErrorBase

--- core/src/main/kotlin/io/github/romainbsl/klogger/core/error/ErrorBase.kt
+++ core/src/main/kotlin/io/github/romainbsl/klogger/core/error/ErrorBase.kt
@@ -2,7 +2,7 @@ package io.github.romainbsl.klogger.core.error
 
 import java.util.*
 
-open class ErrorBase (val code: String, val message: String, var innerError: InnerError? = null) {
+open class ErrorBase(val code: String, val message: String, var innerError: InnerError? = null) {
     var details: LinkedList<ErrorBase>? = null
         private set
 
@@ -16,6 +16,29 @@ open class ErrorBase (val code: String, val message: String, var innerError: Inn
         innerError?.setOuterError(error)
         innerError = error
     }
+
+    fun invertInnerError() {
+        innerError = innerError?.invert()
+    }
+}
+
+open class ImmutableErrorBase(val code: String, val message: String) {
+    var innerError: ImmutableInnerError? = null
+        private set
+    var details: LinkedList<ErrorBase>? = null
+        private set
+
+    fun addDetail(detail: ErrorBase) {
+        if (details == null) details = LinkedList<ErrorBase>()
+        (details as LinkedList<ErrorBase>).add(detail)
+    }
+
+    infix fun inner(error: ImmutableInnerError) {
+        if (this.innerError != null )
+            error inner this.innerError as ImmutableInnerError
+    }
+
+    infix fun outer(error: ImmutableInnerError) = this.innerError?.outer(error)
 
     fun invertInnerError() {
         innerError = innerError?.invert()
