b67b917ffb79d16e1470fd9fcc96a71335009ae9
Romain Bsl <romainbsl@users.noreply.github.com>
2017-06-10 16:35:39 +0200

    make ErrorBase Cloneable

--- core/src/main/kotlin/io/github/romainbsl/kerror/core/error/ErrorBase.kt
+++ core/src/main/kotlin/io/github/romainbsl/kerror/core/error/ErrorBase.kt
@@ -1,46 +1,35 @@
 package io.github.romainbsl.kerror.core.error
 
 import java.util.*
+import kotlin.reflect.KFunction2
 
-open class ErrorBase(val code: String, val message: String) {
+open class ErrorBase(val code: String, val message: String) : Cloneable {
     var innerError: InnerError? = null
         private set
-    var details: LinkedList<ErrorBase>? = null
+    var details = LinkedList<ErrorBase>()
         private set
 
-    fun addDetail(detail: ErrorBase) {
-        if (details == null) details = LinkedList<ErrorBase>()
-        (details as LinkedList<ErrorBase>).add(detail)
-    }
+    infix fun inner(error: InnerError) = testInnerOuter(error, InnerError::inner)
+    infix fun outer(error: InnerError) = testInnerOuter(error, InnerError::outer)
 
-    infix fun inner(error: InnerError) {
-        if (this.innerError != null) error inner this.innerError!!
-    }
+    private fun testInnerOuter(error: InnerError,
+                               method: KFunction2<InnerError,
+                                       @ParameterName(name = "error") InnerError, InnerError>) {
+        val localInnerError = innerError
 
-    infix fun outer(error: InnerError) = this.innerError?.outer(error)
+        innerError = when {
+            localInnerError == null -> error
+            method == InnerError::inner -> method.invoke(error, localInnerError)
+            method == InnerError::outer -> method.invoke(localInnerError, error)
+            else -> throw IllegalArgumentException()
+        }
+    }
 
     fun invertInnerError() {
         innerError = innerError?.invert()
+    }
+
+    override public fun clone(): ErrorBase {
+        return super.clone() as ErrorBase
     }
 }
-
-/*
-open class ErrorBase(val code: String, val message: String, var innerError: InnerError? = null) {
-    var details: LinkedList<ErrorBase>? = null
-        private set
-
-    fun addDetail(detail: ErrorBase?) {
-        if (detail == null) return
-        if (details == null) details = LinkedList<ErrorBase>()
-        (details as LinkedList<ErrorBase>).add(detail)
-    }
-
-    fun setOuterError(error: InnerError) {
-        innerError?.setOuterError(error)
-        innerError = error
-    }
-
-    fun invertInnerError() {
-        innerError = innerError?.invert()
-    }
-}*/
